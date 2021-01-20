8848845b522e2fccf14379fc9b9f95e17d3add6c
Enshinyan <emeka.enshinyan@gmail.com>
2021-01-19 22:55:34 -0800

    scope change of initViews() and ValidateData() methods

--- app/src/main/java/com/example/android_layout_register_exercise/MainActivity.java
+++ app/src/main/java/com/example/android_layout_register_exercise/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
     private RadioButton rbMale, rbFemale, rbOther;
     private CheckBox chkAgree;
     private Button btnRegister, btnImage;
-    private Spinner spinnerC;
+    private Spinner spinnerCountries;
     private TextView txtNameWarn, txtEmailWarn, txtPasswordWarn, txtPasswordReenterWarn;
     private ConstraintLayout parent;
 
@@ -40,8 +40,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        //initialize all UI elements
         initViews();
+
 
         btnImage.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -53,20 +54,24 @@ public class MainActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "register button", Toast.LENGTH_SHORT).show();
                 initRegister();
             }
 
-            private void initRegister() {
-                Log.d(TAG, "initRegister Started:");
+        });
+    }
+    //checks if user has entered all the relevant data
+    private void initRegister() {
+        Log.d(TAG, "initRegister Started:");
 
-                if (validateData()){
-                    if (chkAgree.isChecked()){
-                        showSnackBar();
-                    }else Toast.makeText(MainActivity.this, "Please agree to the terms", Toast.LENGTH_SHORT).show();
-                }
-            }
-            private void showSnackBar(){
+        if (validateData()) {
+            if (chkAgree.isChecked()) {
+                //showSnackBar();
+            } else
+                Toast.makeText(MainActivity.this, "Please agree to the terms", Toast.LENGTH_SHORT).show();
+        }
+    }
+    //shows snackBar
+    private void showSnackBar(){
                 Log.d(TAG, "showSnackBar: Started");
                 txtNameWarn.setVisibility(View.GONE);
                 txtEmailWarn.setVisibility(View.GONE);
@@ -80,66 +85,56 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }).show();
             }
-            private boolean validateData(){
-                Log.d(TAG, "initRegister");
-                if (txtName.getText().toString().equals("")){
-                    txtNameWarn.setVisibility(View.VISIBLE);
-                    txtNameWarn.setText("Enter your name");
-                    return false;
-                }
-                if (txtEmail.getText().toString().equals("")){
-                    txtEmailWarn.setVisibility(View.VISIBLE);
-                    txtEmailWarn.setText("Please enter your email");
-                    return false;
-                }
-                if (txtPassword.getText().toString().equals("")){
-                    txtPasswordWarn.setVisibility(View.VISIBLE);
-                    txtPasswordWarn.setText("please enter your password");
-                    return false;
-                }
-                if (txtPasswordReenter.getText().toString().equals("")){
-                    txtPasswordReenterWarn.setVisibility(View.VISIBLE);
-                    txtPasswordReenterWarn.setText("Please reenter your password");
-                    return false;
-                }
-                if (txtPassword.getText() != txtPasswordReenter.getText()){
-                    txtPasswordReenterWarn.setVisibility(View.VISIBLE);
-                    txtPasswordReenterWarn.setText("Make sure both passwords match");
-                    return false;
-                }
-                return true;
-            }
-        });
+    //ensures that btnRegister will work only if all relevant info are inputted correctly
+    private boolean validateData() {
+        Log.d(TAG, "initRegister");
+        if (txtName.getText().toString().equals("")) {
+            txtNameWarn.setVisibility(View.VISIBLE);
+            txtNameWarn.setText("Enter your name");
+            return false;
+        }
+        if (txtEmail.getText().toString().equals("")) {
+            txtEmailWarn.setVisibility(View.VISIBLE);
+            txtEmailWarn.setText("Please enter your email");
+            return false;
+        }
+        if (txtPassword.getText().toString().equals("")) {
+            txtPasswordWarn.setVisibility(View.VISIBLE);
+            txtPasswordWarn.setText("please enter your password");
+            return false;
+        }
+        if (txtPasswordReenter.getText().toString().equals("")) {
+            txtPasswordReenterWarn.setVisibility(View.VISIBLE);
+            txtPasswordReenterWarn.setText("Please reenter your password");
+            return false;
+        }
+        if (txtPassword.getText() != txtPasswordReenter.getText()) {
+            txtPasswordReenterWarn.setVisibility(View.VISIBLE);
+            txtPasswordReenterWarn.setText("Make sure both passwords match");
+            return false;
+        }
+        return true;
+    }
+        //all vies set to be initialized
+        private void initViews () {
+            Log.d(TAG, "initViews: Started");
+            txtName = findViewById(R.id.txtName);
+            txtEmail = findViewById(R.id.txtEmail);
+            txtPassword = findViewById(R.id.txtPassword);
+            txtPasswordReenter = findViewById(R.id.txtPasswordReenter);
 
+            rgGender = findViewById(R.id.rgGender);
+            rbMale = findViewById(R.id.rbMale);
 
+            chkAgree = findViewById(R.id.chkAgree);
 
+            btnImage = findViewById(R.id.btnImage);
+            btnRegister = findViewById(R.id.btnRegister);
 
+            spinnerCountries = findViewById(R.id.spinnerCountries);
 
+            parent = findViewById(R.id.parent);
+
+        }
 
     }
-
-    private void initViews(){
-        Log.d(TAG, "initViews: Started");
-        txtName = findViewById(R.id.txtName);
-        txtEmail = findViewById(R.id.txtEmail);
-        txtPassword = findViewById(R.id.txtPassword);
-        txtPasswordReenter = findViewById(R.id.txtPasswordReenter);
-
-        rgGender = findViewById(R.id.rgGender);
-        rbMale = findViewById(R.id.rbMale);
-
-        chkAgree = findViewById(R.id.chkAgree);
-
-        btnImage = findViewById(R.id.btnImage);
-        btnRegister = findViewById(R.id.btnRegister);
-
-        parent = findViewById(R.id.parent);
-
-        Spinner spinner = (Spinner) findViewById(R.id.spinnerCountries);
-
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.spinner, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(adapter);
-    }
-}
